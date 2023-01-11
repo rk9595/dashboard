c2867f858b2e4314cd93e685dde25fab105dd94e
rk9595 <83279947+rk9595@users.noreply.github.com>
2023-01-11 07:12:55 +0530

    feat: frontend setup

--- server/index.js
+++ server/index.js
@@ -11,20 +11,20 @@ import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
 // data imports
-import User from './models/User.js';
-import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js';
-import Transaction from './models/Transaction.js';
-import OverallStat from './models/OverallStat.js';
-import AffiliateStat from './models/AffiliateStat.js';
-import {
-  dataUser,
-  dataProduct,
-  dataProductStat,
-  dataTransaction,
-  dataOverallStat,
-  dataAffiliateStat,
-} from './data/index.js';
+// import User from './models/User.js';
+// import Product from './models/Product.js';
+// import ProductStat from './models/ProductStat.js';
+// import Transaction from './models/Transaction.js';
+// import OverallStat from './models/OverallStat.js';
+// import AffiliateStat from './models/AffiliateStat.js';
+// import {
+//   dataUser,
+//   dataProduct,
+//   dataProductStat,
+//   dataTransaction,
+//   dataOverallStat,
+//   dataAffiliateStat,
+// } from './data/index.js';
 
 /* CONFIGURATION */
 dotenv.config();
