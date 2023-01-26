87fd20c60c3abf9c1a03ad69ae1930d30c36d1be
rk9595 <83279947+rk9595@users.noreply.github.com>
2023-01-26 21:19:12 +0530

    feat: Productmodel

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from 'state';
 import { Provider } from 'react-redux';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from 'state/api';
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +25,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- server/index.js
+++ server/index.js
@@ -11,20 +11,13 @@ import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
 // data imports
-// import User from './models/User.js';
+import User from './models/User.js';
 // import Product from './models/Product.js';
 // import ProductStat from './models/ProductStat.js';
 // import Transaction from './models/Transaction.js';
 // import OverallStat from './models/OverallStat.js';
 // import AffiliateStat from './models/AffiliateStat.js';
-// import {
-//   dataUser,
-//   dataProduct,
-//   dataProductStat,
-//   dataTransaction,
-//   dataOverallStat,
-//   dataAffiliateStat,
-// } from './data/index.js';
+import { dataUser } from './data/index.js';
 
 /* CONFIGURATION */
 dotenv.config();
@@ -59,6 +52,6 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
-    // User.insertMany(dataUser);
+    User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
