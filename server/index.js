6fa4d2dbd0a93fc100a81ce44edf69f7ec73e4f0
rk9595 <83279947+rk9595@users.noreply.github.com>
2023-01-28 19:24:27 +0530

    feat: addingCharts

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ import salesRoutes from './routes/sales.js';
 
 // data imports
 import User from './models/User.js';
-// import Product from './models/Product.js';
-// import ProductStat from './models/ProductStat.js';
-// import Transaction from './models/Transaction.js';
-// import OverallStat from './models/OverallStat.js';
-// import AffiliateStat from './models/AffiliateStat.js';
+import Product from './models/Product.js';
+import ProductStat from './models/ProductStat.js';
+import Transaction from './models/Transaction.js';
+import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
 import { dataUser } from './data/index.js';
 
 /* CONFIGURATION */
@@ -52,6 +52,6 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
-    User.insertMany(dataUser);
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
